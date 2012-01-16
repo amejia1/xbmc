15bcd715ba20be35d8cb16adc6bb9302a25b3444
Andres Mejia <amejia@xbmc.org>
2012-01-16 18:15:56 -0500

    Take advantage of CFileStream and TiXmlDocument::StreamIn to read XML from streams.

--- xbmc/utils/XBMCTinyXML.cpp
+++ xbmc/utils/XBMCTinyXML.cpp
@@ -41,7 +41,7 @@ bool CXBMCTinyXML::LoadFile(const char *_filename, TiXmlEncoding encoding)
   TIXML_STRING filename( _filename );
   value = filename;
 
-  XFILE::CFile file;
+  XFILE::CFileStream file;
   if (!file.Open(value))
   {
     SetError( TIXML_ERROR_OPENING_FILE, 0, 0, TIXML_ENCODING_UNKNOWN );
@@ -52,119 +52,10 @@ bool CXBMCTinyXML::LoadFile(const char *_filename, TiXmlEncoding encoding)
   Clear();
   location.Clear();
 
-  // Get the file size, so we can pre-allocate the string. HUGE speed impact.
-  long length = -1;
-  int64_t filelen = file.GetLength();
-  if (filelen > 0)
-    length = (long)filelen;
-
-  // We might be streaming it, correct length will be fixed by reading
-  if( length < 0 )
-    length = 1024;
-
-  // Subtle bug here. TinyXml did use fgets. But from the XML spec:
-  // 2.11 End-of-Line Handling
-  // <snip>
-  // <quote>
-  // ...the XML processor MUST behave as if it normalized all line breaks in external
-  // parsed entities (including the document entity) on input, before parsing, by translating
-  // both the two-character sequence #xD #xA and any #xD that is not followed by #xA to
-  // a single #xA character.
-  // </quote>
-  //
-  // It is not clear fgets does that, and certainly isn't clear it works cross platform.
-  // Generally, you expect fgets to translate from the convention of the OS to the c/unix
-  // convention, and not work generally.
-
-  /*
-  while( fgets( buf, sizeof(buf), file ) )
-  {
-    data += buf;
-  }
-  */
-
-  char*  buf = (char*)malloc(length+1);
-  long   pos = 0;
-  long   len;
-  while( (len = file.Read(buf+pos, length-pos)) > 0 )
-  {
-    pos += len;
-    assert(pos <= length);
-    if(pos == length)
-    {
-      length *= 2;
-      buf = (char*)realloc(buf, length);
-    }
-  }
-  length = pos;
-
-  file.Close();
-
-  // Strange case, but good to handle up front.
-  if ( length == 0 )
-  {
-    SetError( TIXML_ERROR_DOCUMENT_EMPTY, 0, 0, TIXML_ENCODING_UNKNOWN );
-    return false;
-  }
-
-  // If we have a file, assume it is all one big XML file, and read it in.
-  // The document parser may decide the document ends sooner than the entire file, however.
   TIXML_STRING data;
-  data.reserve( length );
-
-  const char* lastPos = buf;
-  const char* p = buf;
-
-  buf[length] = 0;
-  while( *p )
-  {
-    assert( p < (buf+length) );
-    if ( *p == 0xa )
-    {
-      // Newline character. No special rules for this. Append all the characters
-      // since the last string, and include the newline.
-      data.append( lastPos, (p-lastPos+1) );  // append, include the newline
-      ++p;                  // move past the newline
-      lastPos = p;              // and point to the new buffer (may be 0)
-      assert( p <= (buf+length) );
-    }
-    else if ( *p == 0xd )
-    {
-      // Carriage return. Append what we have so far, then
-      // handle moving forward in the buffer.
-      if ( (p-lastPos) > 0 )
-      {
-        data.append( lastPos, p-lastPos );  // do not add the CR
-      }
-      data += (char)0xa;            // a proper newline
-
-      if ( *(p+1) == 0xa )
-      {
-        // Carriage return - new line sequence
-        p += 2;
-        lastPos = p;
-        assert( p <= (buf+length) );
-      }
-      else
-      {
-        // it was followed by something else...that is presumably characters again.
-        ++p;
-        lastPos = p;
-        assert( p <= (buf+length) );
-      }
-    }
-    else
-    {
-      ++p;
-    }
-  }
-  // Handle any left over characters.
-  if ( p-lastPos )
-  {
-    data.append( lastPos, p-lastPos );
-  }
-  free(buf);
-  buf = 0;
+  data.reserve(8 * 1000);
+  StreamIn(&file, &data);
+  file.Close();
 
   Parse(data.c_str(), 0, encoding);
 
